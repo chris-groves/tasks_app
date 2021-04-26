f3531d6220091871048a4f522cce0b4f895cd661
Chris Groves <chris_groves87@hotmail.co.uk>
2021-04-26 09:16:14 +0100

    Remove 'actions' routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,4 @@ Rails.application.routes.draw do
   get '/tasks', to: "tasks#index"
   get '/tasks/new', to: "tasks#new"
   post '/tasks', to: "tasks#create"
-
-  get "actions", to: "actions#index"
-  get "/actions/new", to: "actions#new"
-  post "/actions", to: "actions#create"
-
 end
