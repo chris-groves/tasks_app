78aa9a21a14550301b3c95bc88c8f2c089dd4472
Chris Groves <chris_groves87@hotmail.co.uk>
2021-04-26 09:07:28 +0100

    Remove actions table

--- db/migrate/20210422161141_create_actions.rb
+++
@@ -1,9 +0,0 @@
-class CreateActions < ActiveRecord::Migration[6.1]
-  def change
-    create_table :actions do |t|
-      t.text :description
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2021_04_23_152142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "actions", force: :cascade do |t|
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
