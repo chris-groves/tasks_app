ce1bc998fd7dc8a89e13c877213788b099241cf0
Chris Groves <chris_groves87@hotmail.co.uk>
2021-05-25 18:47:56 +0100

    Delete a task

--- spec/features/show_page_spec.rb
+++ spec/features/show_page_spec.rb
@@ -9,4 +9,14 @@ feature 'show page' do
 
     expect(page).to have_content('Read a book')
   end
+
+  scenario 'delete a task' do
+    visit('/tasks/new')
+    fill_in('Description', with: 'Read a book')
+    click_on('Create Task')
+    click_on('Read a book')
+    click_on('Delete')
+
+    expect(page).to_not have_content('Read a book')
+  end
 end
