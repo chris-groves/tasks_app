86b4236786e0d28feb8b2e97959159e14c1051ea
Chris Groves <chris_groves87@hotmail.co.uk>
2021-05-28 16:03:41 +0100

    Add request spec for creating new task

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -14,4 +14,13 @@ RSpec.describe "Tasks", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "POST /tasks" do
+    subject(:create_new_task) { post tasks_path, params: { task: { description: "Read" } } }
+
+    it "creates a new task" do
+      create_new_task
+      expect(create_new_task).to redirect_to(tasks_path)
+    end
+  end
 end
