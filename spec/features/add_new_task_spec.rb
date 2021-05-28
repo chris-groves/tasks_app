33e40c78da6a44c0ad74a0d0dfe1ee9804cc7765
Chris Groves <chris_groves87@hotmail.co.uk>
2021-05-28 15:41:37 +0100

    Add validation to ensure task cannot be created with a blank description

--- spec/features/add_new_task_spec.rb
+++ spec/features/add_new_task_spec.rb
@@ -9,6 +9,14 @@ feature "new task" do
     expect(page).to have_content("Read a book")
   end
 
+  scenario "fail to add a new task item" do
+    visit("/tasks/new")
+    # fill_in("Description", with: "Read a book")
+    click_on("Create Task")
+
+    expect(page).to have_content("Description can't be blank")
+  end
+
   scenario "link to index page" do
     visit("/tasks/new")
     click_on("Index Page")
