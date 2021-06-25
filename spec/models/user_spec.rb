84a83cb3dc347bc37b1188bea7d4aec5fb33b3e9
Chris Groves <chris_groves87@hotmail.co.uk>
2021-06-25 19:06:40 +0100

    Add password to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
   end
 end
