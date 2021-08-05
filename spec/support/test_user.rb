27f126484dd892ec4ad6ba366ccb9b4b1922d94f
Chris Groves <chris_groves87@hotmail.co.uk>
2021-08-05 12:06:36 +0100

    Move creation of test users into separate class

--- spec/support/test_user.rb
+++ spec/support/test_user.rb
@@ -10,6 +10,42 @@ class TestUser
     click_on("Sign Up")
   end
 
+  def create_without_name
+    visit("/users/new")
+    fill_in("Name", with: "")
+    fill_in("Email", with: "me@example.com")
+    fill_in("Password", with: "foobar")
+    fill_in("Password confirmation", with: "foobar")
+    click_on("Sign Up")
+  end
+
+  def create_without_email
+    visit("/users/new")
+    fill_in("Name", with: "Example")
+    fill_in("Email", with: "")
+    fill_in("Password", with: "foobar")
+    fill_in("Password confirmation", with: "foobar")
+    click_on("Sign Up")
+  end
+
+  def create_without_password
+    visit("/users/new")
+    fill_in("Name", with: "Example")
+    fill_in("Email", with: "")
+    fill_in("Password", with: "")
+    fill_in("Password confirmation", with: "")
+    click_on("Sign Up")
+  end
+
+  def create_with_short_password
+    visit("/users/new")
+    fill_in("Name", with: "Example")
+    fill_in("Email", with: "")
+    fill_in("Password", with: "foo")
+    fill_in("Password confirmation", with: "foo")
+    click_on("Sign Up")
+  end
+
   def log_in
     visit("/sessions/new")
     fill_in("Email", with: "me@example.com")
