8c4a8b49e3bcdf974ab1e2094df6fed690727447
Chris Groves <chris_groves87@hotmail.co.uk>
2021-08-06 10:39:38 +0100

    Add controller tests for log out functionality

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -21,4 +21,26 @@ describe SessionsController, type: :controller do
       end
     end
   end
+
+  describe "DELETE destroy" do
+    it "redirects to homepage" do
+      User.create(name: "Chris", email: "me@example.com", password: "foobar",
+                  password_confirmation: "foobar")
+      post :create, params: { session: { email: "me@example.com",
+                                        password: "foobar" } }
+      delete :destroy, params: { id: session[:user_id] }
+
+      expect(response).to redirect_to(root_path)
+    end
+
+    it "detroys the session" do
+      user = User.create(name: "Chris", email: "me@example.com",
+                         password: "foobar", password_confirmation: "foobar")
+      post :create, params: { session: { email: "me@example.com",
+                                        password: "foobar" } }
+      delete :destroy, params: { id: session[:user_id] }
+      
+      expect(session[:user_id]).to eq(nil)
+    end
+  end
 end
